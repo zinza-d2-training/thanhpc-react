6e3e34c73d1cbe78be2e46c5d5537db4cd7787f9
cthanh2310 <76828456+cthanh2310@users.noreply.github.com>
2022-01-27 17:00:07 +0700

    Login Layout (#11)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
 import './App.css';
-import { CssBaseline, Typography } from '@mui/material';
 import Layout from './theme/layout';
 
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-
-      <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Layout>
     </>
   );
 }
