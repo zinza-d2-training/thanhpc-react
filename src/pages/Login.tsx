f18419f4807ae49229b01344255da42af2661c0d
Thanh pham <thanhpham.23102000@gmail.com>
2022-01-27 09:33:32 +0700

    Login Layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,28 @@
-const Login = (): any=>{
-    return(
-        <div>Login page</div>
-    ) 
-}
+import { Box, Grid } from '@mui/material';
+import loginImg from '../images/login.png';
+import styled from '@emotion/styled';
+
+const Login = (): any => {
+  const Button = styled.button`
+    color: red;
+  `;
+  return (
+    <Box>
+      <Grid container spacing={0}>
+        <Grid item xs={6}>
+          <Box
+            component="img"
+            sx={{
+              height: '100%',
+              width: '100%'
+            }}
+            alt=""
+            src={loginImg}
+          />
+        </Grid>
+        <Grid item xs={6}></Grid>
+      </Grid>
+    </Box>
+  );
+};
 export default Login;
