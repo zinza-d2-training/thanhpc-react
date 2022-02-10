31f8c852bb523d2221e2439e443f0482cff2cf72
hungnp194 <61738572+hungnp194@users.noreply.github.com>
2022-02-11 02:48:16 +0700

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Layout from './theme/layout';
 
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import User from './pages/User';
@@ -10,14 +11,14 @@ function App() {
   return (
     <>
       <Layout>
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/forgot-password" element={<ForgotPassword />}></Route>
             <Route path="/" element={<Home />}></Route>
             <Route path="/user" element={<User />}></Route>
           </Routes>
-        </BrowserRouter>
+        </Router>
       </Layout>
     </>
   );
