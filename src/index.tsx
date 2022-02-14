4fab3604bcc8b64de0fdc61d05b26d876189983b
cthanh2310 <76828456+cthanh2310@users.noreply.github.com>
2022-02-14 12:25:41 +0700

    Implement forgot password (#19)
    * Implement forgot password: custom hooks(useClock) + use modal component from mui
    
    * fix conflict
    
    * Implement forgot password: custom hooks(useClock) + use modal component from mui
    
    * Deploy (#17)
    
    * Fix heroku (#18)
    
    * remove file package-lock.json. Use yarn instead of useing npm
    
    * reinstall yarn
    
    * fix bug about react-router
    
    * fix conflict
    
    * Update yarn.lock
    
    Co-authored-by: hungnp194 <61738572+hungnp194@users.noreply.github.com>
    Co-authored-by: hungnp <hungnp194@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
